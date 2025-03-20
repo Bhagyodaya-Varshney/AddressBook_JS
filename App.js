95dca03e7f3900787f197e51e634626b07a37798
Bhagyodaya_Varshney <mtrbhagyodayame@email.com>
2025-03-20 21:41:17 +0530

    UC-4 Ability to find existing contact person using their name and edit it DONE

--- App.js
+++ App.js
@@ -45,12 +45,7 @@ class Contact {
     static validateEmail(email) {
         return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
     }
-
-    displayContact() {
-        return `${this.firstName} ${this.lastName}, ${this.address}, ${this.city}, ${this.state}, ${this.zip}, Phone: ${this.phone}, Email: ${this.email}`;
-    }
 }
-
 
 class AddressBook {
     constructor() {
@@ -70,6 +65,16 @@ class AddressBook {
     displayContacts() {
         return this.contacts.map(contact => contact.displayContact());
     }
+
+    findAndEditContact(name, updatedDetails) {
+        const contact = this.contacts.find(c => c.firstName === name || c.lastName === name);
+        if (contact) {
+            Object.assign(contact, updatedDetails);
+            console.log('Contact updated successfully:', contact);
+        } else {
+            console.error('Contact not found.');
+        }
+    }
 }
 
 const addressBook = new AddressBook();
@@ -81,4 +86,10 @@ addressBook.addContact("Anuj", "choudhary", "Mathura", "CityName", "UttarPradesh
 addressBook.addContact("Swapnil", "Sharma", "Mathura", "CityName", "UttarPradesh", "281001", "1234567890", "Swapnil@gmail.com");
 addressBook.addContact("Arush", "kumar", "Mathura", "CityName", "UttarPradesh", "281001", "1234567890", "Arush@gmail.com");
 console.log(JSON.stringify(addressBook.contacts));
+console.log();
+
+addressBook.findAndEditContact("Xyz",{city: "NewCity", phone: "9876543210"});
+console.log(JSON.stringify(addressBook.contacts,null,2));
+
+
 console.log();
